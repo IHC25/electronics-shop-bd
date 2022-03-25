9d6458c1c5e3217b096a3622633c0656f9ce9670
arefin6 <imranhchy@gmail.com>
2022-03-26 02:53:46 +0600

    shop and product component added

---
+++ src/components/Product/Product.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import "./Product.css";
+
+const Product = (props) => {
+  const { name, price, img } = props.product;
+  return (
+    <div className="product">
+      <img src={img} alt="" />
+      <div className="product-info">
+        <p className="product-name">{name}</p>
+        <p>Price: {price}৳</p>
+      </div>
+      <button className="btn-cart">
+        <p>Add To Cart</p>
+        <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+      </button>
+    </div>
+  );
+};
+
+export default Product;
