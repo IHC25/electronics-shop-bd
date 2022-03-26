ae61cea1751cf4a256e9780afae5472ace1b1154
arefin6 <imranhchy@gmail.com>
2022-03-26 17:15:57 +0600

    choose 1 button handled

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import CartItem from "../CartItem/CartItem";
 import "./Cart.css";
 
 const Cart = (props) => {
   const { carts } = props;
+  const [suggestion, setSuggestion] = useState([]);
+  const chooseButtonHandle = (items) => {
+    const chosen = items[Math.floor(Math.random() * items.length)];
+    setSuggestion(chosen);
+  };
   return (
     <div className="cart">
       <h3>Shopping Cart</h3>
       {carts.map((cart) => (
         <CartItem cart={cart}></CartItem>
       ))}
+      <button onClick={() => chooseButtonHandle(carts)} className="choose-btn">
+        Choose 1 For Me
+      </button>
+      <h3>Buy Suggestion:</h3>
+      <div className="buy-suggestion">
+        <p>
+          <small>{suggestion.name}</small>
+        </p>
+      </div>
     </div>
   );
 };
